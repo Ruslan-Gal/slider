50116a33d0cb9e12f24e8ba9266d313279391390
Ruslan <rhgalyamov@gmail.com>
2021-06-16 13:59:07 +0300

    black and white drop-down cards

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
 function slidesPlugin(activeSlideNum = 2) {
   const slides = document.querySelectorAll('.slide');
 
-  slides[activeSlideNum].classList.add('active');
+  slides[activeSlideNum].classList.add('active', 'color');
 
   slides.forEach((slide) => {
     slide.addEventListener('click', () => {
       clearActiveClasses();
 
-      slide.classList.add('active');
+      slide.classList.add('active', 'color');
     });
   });
 
   function clearActiveClasses() {
     slides.forEach((slide) => {
-      slide.classList.remove('active');
+      slide.classList.remove('active', 'color');
     });
   }
 }
